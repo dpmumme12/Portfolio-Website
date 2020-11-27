605de93be538f777b714497382f3797b7fd3379a
Doug Mumme <dougmumme@gmail.com>
2020-11-26 19:19:50 -0600

    php

--- public/php/mail.php
+++ public/php/mail.php
@@ -8,7 +8,7 @@
     $text= $_POST["message"];
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . <$email> . "\r\n"; // Sender's E-mail
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">

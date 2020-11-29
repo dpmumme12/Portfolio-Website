2a541eb2723263e02a1a139ba3bebfe292012064
Doug Mumme <dougmumme@gmail.com>
2020-11-28 19:12:02 -0600

    php

--- public/php/mail.php
+++ public/php/mail.php
@@ -3,55 +3,19 @@
 
 
     $to = 'dougmumme@gmail.com'; // Replace this Mail ID with yours
-    $websiteURL = "https://douglasmumme.herokuapp.com/"; // Replace Your Website URL for Show Logo
-	$websiteName = "douglasmumme"; // Replace Your Website Name for Show Logo alt Text.
 	
 	$name = $_POST["name"];
     $email = $_POST["email"];
     $text= $_POST["message"];
-
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
-  <tr>
-    <td align="center" valign="top"><table width="600" bgcolor="#eee" valign="top" style="font-size:17px; padding:25px;">
-        <tr>
-          <td style="text-align:center;" width="100" valign="top"><img src="'.$websiteURL.'/images/logo.png" alt="'.$websiteName.'"/></td>
-        </tr>
-      </table></td>
-  </tr>
-  <tr>
-    <td align="center" valign="top">
-	<table width="600" bgcolor="#f8f6fe" cellpadding="7" style="font-size:16px; padding:30px; line-height: 28px;">
-        <tr>
-            <td style="text-align:right; padding-right: 20px;" width="25%" valign="top"><strong>Name:</strong></td>
-			<td>'.$name.'</td>
-        </tr>
-        <tr>
-			<td style="text-align:right; padding-right: 20px;" valign="top"><strong>Email:</strong></td>
-			<td>'.$email.'</td>
-		</tr>
-        <tr>
-			<td style="text-align:right; padding-right: 20px;" valign="top"><strong>Message:</strong></td>
-			<td>'.$text.'</td>
-		</tr>
-    </table>
-	</tr>
-	</td>
-    </table>';
     
     $from = new SendGrid\Email(null, $email);
-    $subject = $headers;
+    $subject = "Message from Portfolio-Site";
     $to = new SendGrid\Email(null, $to);
-    $content = new SendGrid\Content("text/html", $message);
+    $content = new SendGrid\Content("text/plain", $text);
     $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
     $apiKey = getenv('SENDGRID_API_KEY');
     $sg = new \SendGrid($apiKey);
-
-    $sg->client->mail()->send()->post($mail);
 	
 	$success = "Thank you for contacting us. We will be in touch with you very soon."; // Success Message Text
     $failed = "Sorry! This message sent is unsuccessful."; // Failed Message Text

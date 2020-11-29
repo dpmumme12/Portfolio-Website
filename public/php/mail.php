3eb466df352e4f23a7a193e3041a28f62704b8d9
Doug Mumme <dougmumme@gmail.com>
2020-11-28 19:01:41 -0600

    php changes

--- public/php/mail.php
+++ public/php/mail.php
@@ -4,14 +4,14 @@
     $from = new SendGrid\Email(null, "dougmumme@gmail.com");
     $subject = "Hello World from the SendGrid PHP Library!";
     $to = new SendGrid\Email(null, "dougmumme@gmail.com");
-    $content = new SendGrid\Content("text/plain", "Hello, Email!");
+    $content = new SendGrid\Content("text/plain", "Hello, you are a great coder!!");
     $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
     $apiKey = getenv('SENDGRID_API_KEY');
     $sg = new \SendGrid($apiKey);
 
-    /*$response = $sg->client->mail()->send()->post($mail);
-    echo $response->statusCode();
+    $response = $sg->client->mail()->send()->post($mail);
+    /*echo $response->statusCode();
     echo $response->headers();
     echo $response->body();*/
 
